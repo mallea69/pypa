b7949b9ab88be29702060a64045ef7ac60b1d002
mallea69 <lizamorozova2709@gmail.com>
2025-11-13 12:46:55 +0500

    jjjj

--- shared/ui.js
+++ shared/ui.js
@@ -1,7 +1,25 @@
+import Image from "next/image"
 
+
+export const Cover = ({ src}) => {
+    return(
+        <Image src = {src} alt="Обложка"/>
+    )
+}
 
 export const Title = ({children}) =>{
     return(
         <span>{children}</span>
+    )
+}
+
+export const Artst = ({arist}) =>{
+    return (
+        <p>{artist}</p>
+    )
+}
+export const Duration = ({Duration})=>{
+    return(
+        <span>{duration}</span>
     )
 }
